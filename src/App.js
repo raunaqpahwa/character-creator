6a225ac3631af18b14f8050ff40868b6c0c869eb
Raunaq Pahwa <raunaqpahwa@gmail.com>
2024-01-08 16:45:05 -0500

    Completed CSS changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import Background from './components/Background';
 
 function App() {
   return (
     <>
+      <Background />
       <CharacterEditor />
       <Footer />
     </>
